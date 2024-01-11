5a32e0ce7a6c7e1331c69de298b34d35fc179c7a
DevViniciusAmaral <viniciusamaraldev@gmail.com>
2024-01-11 17:03:42 -0300

    feat: configurado caminhos absoluto

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,22 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo']
+    presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          root: ["./src"],
+          alias: {
+            "@infrastructure": "./src/infrastructure",
+            "@assets": "./src/application/assets",
+            "@components": "./src/application/components",
+            "@hooks": "./src/application/hooks",
+            "@screens": "./src/application/screens",
+            "@utils": "./src/application/utils",
+          },
+        },
+      ],
+    ],
   };
 };
