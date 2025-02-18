86f2e82a61bcb4e56ed09fef7392fb5725be3963
DevViniciusAmaral <dev.viniciusamaral@gmail.com>
2025-02-18 20:02:37 -0300

    refactor: ajuste na interface IRepository

--- src/application/models/IRepository.ts
+++ src/application/models/IRepository.ts
@@ -1,14 +1,16 @@
+export interface IRepositoryData {
+  id: number;
+  name: string;
+  description: string;
+  html_url: string;
+  created_at: string;
+  updated_at: string;
+}
+
 export interface IRepository {
   owner: {
     avatar_url: string;
     login: string;
   };
-  data: Array<{
-    id: number;
-    name: string;
-    description: string;
-    html_url: string;
-    created_at: string;
-    updated_at: string;
-  }>;
+  data: IRepositoryData[];
 }
