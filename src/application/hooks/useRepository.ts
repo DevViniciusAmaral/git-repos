3bbc922688a5a1705086b22477308a2e72775e3d
DevViniciusAmaral <dev.viniciusamaral@gmail.com>
2025-02-18 14:28:00 -0300

    refactor

--- src/application/hooks/useRepository.ts
+++ src/application/hooks/useRepository.ts
@@ -5,35 +5,44 @@ import { IRepository } from "../models/IRepository";
 export const useRepository = () => {
   const { repositories, setRepositories } = repositoryStore();
 
-  const findRepository = async (owner: string): Promise<IRepository[]> => {
+  const findRepository = async (owner: string) => {
     try {
       const { data } = await service.list({ owner });
-      return data?.map((repo) => ({
-        id: repo?.id,
-        name: repo?.name,
-        description: repo?.description,
-        owner: repo?.owner,
-        html_url: repo?.html_url,
-        created_at: repo?.created_at,
-        updated_at: repo?.updated_at,
-      }));
+      return {
+        owner: data?.at(0)?.owner,
+        data: data?.map((repo) => ({
+          id: repo?.id,
+          name: repo?.name,
+          description: repo?.description,
+          owner: repo?.owner,
+          html_url: repo?.html_url,
+          created_at: repo?.created_at,
+          updated_at: repo?.updated_at,
+        })),
+      };
     } catch (error) {
       console.log("ERROR [findRepository]: ", error);
     }
   };
 
   const saveRepository = (data: IRepository) => {
-    const isExists = repositories.find((repo) => repo.id === data.id);
+    const isExists = repositories.find(
+      (repository) => repository.owner.login === data.owner.login
+    );
 
-    if (isExists) {
+    if (!isExists) setRepositories([...repositories, data]);
+    else {
       setRepositories(
-        repositories.map((repo) => (repo.id === data.id ? data : repo))
+        repositories.map((repository) => {
+          if (repository.owner.login === data.owner.login) return data;
+          else return repository;
+        })
       );
-    } else setRepositories([...repositories, data]);
+    }
   };
 
-  const deleteRepository = (id: number) => {
-    setRepositories(repositories.filter((repo) => repo.id !== id));
+  const deleteRepository = (owner: string) => {
+    setRepositories(repositories.filter((repo) => repo.owner.login !== owner));
   };
 
   return { repositories, findRepository, saveRepository, deleteRepository };
--- src/application/models/IRepository.ts
+++ src/application/models/IRepository.ts
@@ -1,12 +1,14 @@
 export interface IRepository {
-  id: number;
-  name: string;
-  description: string;
-  html_url: string;
-  created_at: string;
-  updated_at: string;
   owner: {
     avatar_url: string;
     login: string;
   };
+  data: Array<{
+    id: number;
+    name: string;
+    description: string;
+    html_url: string;
+    created_at: string;
+    updated_at: string;
+  }>;
 }
